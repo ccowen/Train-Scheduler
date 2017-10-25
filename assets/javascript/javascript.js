e83146f4fade225efa2140c6e29fea4207a4a1d5
ccowen <caitlincowen2016@u.northwestern.edu>
2017-10-25 11:51:31 -0500

    setInterval

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,5 @@
-
 // Create a variable to reference the database.
 var database = firebase.database();
-
 
 // Initial Values
 var name = "";
@@ -37,122 +35,115 @@ $("#addEmployee").on("click", function(event) {
 
 });
 
+// function for intial firebase loading
+function getData() {
 
-// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+	database.ref().on("child_added" , function(childSnapshot) {
 
-database.ref().on("child_added", function(childSnapshot) {
+		// Store everything into a variable.
+		var name = childSnapshot.val().name;
+		var destination = childSnapshot.val().destination;
+		var firstTime = childSnapshot.val().firstTime;
+		var frequency = childSnapshot.val().frequency;
 
-	// console.log(childSnapshot.val());
+		var nextArrival;
+		var miutesAway;
 
-	// Store everything into a variable.
-	var name = childSnapshot.val().name;
-	var destination = childSnapshot.val().destination;
-	var firstTime = childSnapshot.val().firstTime;
-	var frequency = childSnapshot.val().frequency;
+		// modify the time format
+		var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
 
-	var nextArrival;
-	var miutesAway;
+		// Current Time
+	    var currentTime = moment();
 
-	// modify the time format
-	var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-	// console.log(firstTimeConverted);
+	    // Difference between the times
+	    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-	// Current Time
-    var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+	    // Time apart (remainder)
+	    var tRemainder = diffTime % frequency;
 
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
+	    // Minute Until Train
+	    var tMinutesTillTrain = frequency - tRemainder;
 
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-    // console.log(tRemainder);
+	    // Next Train
+	    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+	    var nextTrainTime = moment(nextTrain).format("hh:mm A");
 
-    // Minute Until Train
-    var tMinutesTillTrain = frequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+	    /* code before added datatable code
+	    function almostHere(time) {
 
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    var nextTrainTime = moment(nextTrain).format("hh:mm A");
+	    	if (time <= 5)
+	    		return "<td class='minutesUntilNextAlmostAlmostHere'>" + tMinutesTillTrain + "</td>";
+	    	else if	(time < 30) {
+				return "<td class='minutesUntilNextAlmostHere'>" + tMinutesTillTrain + "</td>"; 
+			} 
+			else if (time >= 30) {
+				return "<td>" + tMinutesTillTrain + "</td>";
+			}
 
-    function almostHere(time) {
+	    };
 
-    	if (time <= 5)
-    		return "<td class='minutesUntilNextAlmostAlmostHere'>" + tMinutesTillTrain + "</td>";
-    	else if	(time < 30) {
-			return "<td class='minutesUntilNextAlmostHere'>" + tMinutesTillTrain + "</td>"; 
-		} 
-		else if (time >= 30) {
-			return "<td>" + tMinutesTillTrain + "</td>";
-		}
+		 full list of items to the well
+		$("#myTableAppendHere").append("<tr><td>" + name + 
+			"</td><td>" + destination +
+			"</td><td>" + frequency +
+			"</td><td>" + nextTrainTime + 
+			"</td>" + almostHere(tMinutesTillTrain) + "</tr>"
+			); */
 
-    };
+		function almostHere(time) {
 
-	// full list of items to the well
-	$("#myTableAppendHere").append("<tr><td>" + name + 
-		"</td><td>" + destination +
-		"</td><td>" + frequency +
-		"</td><td>" + nextTrainTime + 
-		"</td>" + almostHere(tMinutesTillTrain) + "</tr>"
-		);
+	    	if (time <= 5) {
+	    		var almostAlmost = tMinutesTillTrain + "<div id='delete'></div><div class='minutesUntilNextAlmostAlmostHere'>Almost Here</div>";
+	    		return almostAlmost;
+	    	}
+	    	else if	(time < 20) {
+	    		var almost = tMinutesTillTrain + "<div id='delete'></div><div class='minutesUntilNextAlmostHere'>In Range</div>";
+	    		return almost;
+			} 
+			else if (time >= 20) {
+				return tMinutesTillTrain + "<div id='delete'></div>";
+			}
 
-	// Handle the errors
-	}, function(errorObject) {
-	console.log("Errors handled: " + errorObject.code);
+	    };
 
-});
+		$('#myTable').DataTable().row.add([
+		  name, destination, frequency, nextTrainTime, almostHere(tMinutesTillTrain)
+		]).draw();
+
+		// Handle the errors
+		}, function(errorObject) {
+		console.log("Errors handled: " + errorObject.code);
+
+	});
+
+};
 
 $(document).ready(function(){
+
+	getData();
+
     $('#myTable').DataTable();
+
+    var table = $('#myTable').DataTable();
+
+    $('#myTable tbody').on( 'click', '#delete', function () {
+	    table
+	        .row( $(this).parents('tr') )
+	        .remove()
+	        .draw();
+	} );
+
+	var myVar = setInterval(function(){ 
+
+		// remove the existing rows from the table
+		var table = $('#myTable').DataTable();
+ 
+		var rows = table
+		    .rows()
+		    .remove()
+		    .draw();
+
+		getData() }, 5000);
+
 });
-
-	/* var $parent = document.getElementsByClassName("minutesUntilNextAlmostHere")[0].parentNode;
-
-	$(parent).addClass(minutesUntilNextAlmostHere);
-
-function sortTable(col,dir){
-
-  var rows = $('#livefeeds tbody  tr').removeClass('row_alt').get();
-  
-  var t=1;
-  if(!dir || dir=='dec') {
-	  t=-1;
-  } else if(dir=='asc') {
-	  t=1;
-  }
-  rows.sort(function(a, b) {
-	  var A = $(a).children('td').eq(col).text().toUpperCase();
-	  var B = $(b).children('td').eq(col).text().toUpperCase();
-	
-	  if(A <b> B) {
-		return t;
-	  }
-	
-	  return 0;
-  });
-
-    $.each(rows, function(index, row) {
-	$('#livefeeds').children('tbody').append(row);
-		if( index%2 == 1){
-			$(row).addClass('row_alt');
-		}
-	
-  }); 
-
- }*/
-
-
-
-
-/*
-setInterval(function(){
-	database.ref().on("child_added", function(childSnapshot) {
-		var frequency = childSnapshot.val().frequency;
-		$("#frequencyId").text(frequency);
-
-	console.log("timer");
-},60000); */
-
 
